785d1483ad3ef5fedf5ba1bde74870e6ebd184b4
master <leges960408@163.com>
2021-06-28 00:45:06 +0800

    one commit

---
+++ src/main/java/text.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: 贺端文
+ * @Date：2021/06/28/0:43
+ * @Description:
+ */
+public class text {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
